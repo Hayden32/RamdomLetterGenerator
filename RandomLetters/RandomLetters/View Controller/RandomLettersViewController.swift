4363e8642a787f3999f5d96231103e36ae47cff1
Hayden Hastings <haydenhastings32@gmail.com>
2019-09-10 17:05:41 -0600

    Got my LAMBDA Constraints

---
+++ RandomLetters/RandomLetters/View Controller/RandomLettersViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RandomLettersViewController.swift
+//  RandomLetters
+//
+//  Created by Hayden Hastings on 9/10/19.
+//  Copyright © 2019 Hayden Hastings. All rights reserved.
+//
+
+import UIKit
+
+class RandomLettersViewController: UIViewController {
+    
+    // MARK: - Properties
+    var isScattered: Bool = true
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func toggleButtonTapped(_ sender: Any) {
+    }
+    
+}
